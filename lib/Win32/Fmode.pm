ee979f9e8f8321ceed2a6b79bbaa85e5d8e06e1e
BrowserUk <browseruk@cpan.org>
2006-03-17 00:54:19 +0000

    Import of BROWSERUK/Win32-Fmode-1.0.4 from CPAN.
    gitpan-cpan-distribution: Win32-Fmode
    gitpan-cpan-version:      1.0.4
    gitpan-cpan-path:         BROWSERUK/Win32-Fmode-1.0.4.tar.gz
    gitpan-cpan-author:       BROWSERUK
    gitpan-cpan-maturity:     released

--- lib/Win32/Fmode.pm
+++ lib/Win32/Fmode.pm
@@ -11,14 +11,18 @@ our @EXPORT = qw(
     fmode
 );
 
-our $VERSION = '1.0.3';
+our $VERSION = '1.0.4';
 
 require XSLoader;
 XSLoader::load('Win32::Fmode', $VERSION);
 
 sub fmode {
     my $fh = shift;
-    return unless fileno( $fh );
+    die "No/bad argument supplied!.\nUsage: fmode( open filehandle )"
+        unless defined $fh and ref $fh eq 'GLOB';
+    die "fmode does no currently work on ramfiles\n" if fileno( $fh ) == -1;
+    die "'$fh' is not an open filehandle\n"
+        unless 1+fileno( $fh );
     return xs_fmode( $fh );
 }
 
@@ -35,22 +39,33 @@ Win32::Fmode - determine whether a Win32 filehandle is opened for reading, writi
  use Win32::Fmode;
  .
  .
- my $mode = fmode( \*HND ); # HND is an open filehandle
+ my $mode = fmode( \*FH ); # FH is an open filehandle
 
 =head1 FUNCTIONS
 
- The purpose is to work around the MS C runtime libraries lack of a
- function to retrieve the file mode used when a file is opened.
+The purpose is to work around the MS C runtime libraries lack of a
+function to retrieve the file mode used when a file is opened.
 
- Exports a single function: fmode
+Exports a single function: fmode
 
- Pass it a Perl filehandle and it will return a numeric value that
- represents the mode parameter used on the open
- (or undef if the filehandle passed invalid or closed).
+Pass it an open Perl filehandle and it will return a numeric value that
+represents the mode parameter used on the open.
+
+If the parameter passed is not an open filehandle, the call will raise an exception.
 
      fmode( \*FILEHANDLE ) &   1 and print "is readonly";
      fmode( \*FILEHANDLE ) &   2 and print "is writeonly";
      fmode( \*FILEHANDLE ) & 128 and print "is read/write";
+
+=head1 BUGS
+
+Note: Ram files c<open FH, '<', \$ram> are not true filehandles (they are tied globs),
+and therefore do not have the associated CRT FILE structure from which this module
+obtains the information, and no way has yet been found to retreive that information
+from them.
+
+The module IO::String suffers teh same limitations.
+
 
 =head1 LICENSE
 
--- t/Win32-Fmode.t
+++ t/Win32-Fmode.t
@@ -13,3 +13,36 @@ BEGIN { use_ok('Win32::Fmode') };
 # Insert your test code below, the Test::More module is use()ed here so read
 # its man page ( perldoc Test::More ) for help writing this test script.
 
+
+## The next three tests shoudl work on a user machine,
+## but test more fails tham and doesn't explain why,
+## so they're disabled also.
+
+#my( $ram, $fh ) = '';
+#
+#open $fh, '<', $0;
+#ok( fmode( $fh ) == 1 );
+#
+#open $fh '>', $0;
+#ok( fmode( $fh ) == 2 );
+#
+#open $fh, '+<', $0;
+#ok( fmode( $fh ) == 128 );
+#
+## The following tests should die, but I couldn't see how to
+## check that using Test::More from a quick scan of the docs
+## so the tests are disabled.
+## (And no, I'm not interested in how I *should* be doing it.
+##  Under development, these Test::More tests are totally useless
+## and at install time, if they detect faults
+## the Perl installation is broken, but if this module detects it,
+## the user's gonna come belating to me to fix it.)
+
+#open $fh, '>', \$ram;
+#fmode( $fh ); ## should die
+#
+close $fh;
+#
+#fmode();
+#fmode( 'junk' );
+#
